9082dcf810ec5f139792cdfa7b52feb1f3ef3b54
Mudream <mudream4869@gmail.com>
2023-09-02 11:01:21 +0800

    Refine logic

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ import io
 import zipfile
 
 
-from typing import BinaryIO, Tuple, List
+from typing import BinaryIO, Tuple, List, Optional, Any
 
 import streamlit as st
 import chardet
@@ -47,6 +47,81 @@ def split_content(content_lines: List[str], reg_title: str) -> List[Chapter]:
     return chapters
 
 
+def build_epub_book(book_title: str,
+                    book_author: str,
+                    book_intro: str,
+                    book_cover: Optional[Any],
+                    chapters: List[Chapter]) -> epub.EpubBook:
+    book = epub.EpubBook()
+    book.set_title(book_title)
+    if book_cover:
+        book.set_cover(book_cover.name, book_cover.read())
+
+    if book_author:
+        book.add_author(book_author)
+
+    book.toc = []
+    spines = []
+
+    if book_intro:
+        intro_filename = 'intro.html'
+        intro_title = 'Introduction'
+        intro_file = epub.EpubHtml(
+            title=intro_title, file_name=intro_filename)
+        intro_file.content = book_intro
+        book.add_item(intro_file)
+        book.toc.append(epub.Link(intro_filename, intro_title))
+        spines.append(intro_file)
+
+    for index, chapter in enumerate(chapters[1:]):
+        ch_filename = f'ch_{index}.html'
+        ch_file = epub.EpubHtml(
+            title=chapter.title, file_name=ch_filename)
+        ch_file.content = f'<h1>{chapter.title}</h1>\n' + \
+            chapter.html_content
+        book.add_item(ch_file)
+
+        book.toc.append(epub.Link(ch_filename, chapter.title))
+
+        spines.append(ch_file)
+
+    book.add_item(epub.EpubNav())
+
+    # define CSS style
+    style = 'BODY {color: white;}'
+    nav_css = epub.EpubItem(
+        uid="style_nav",
+        file_name="style/nav.css",
+        media_type="text/css",
+        content=style,
+    )
+
+    book.add_item(nav_css)
+
+    # basic spine
+    book.spine = ['nav'] + spines
+
+    return book
+
+
+def write_epub(book: epub.EpubBook, buffer: io.BytesIO):
+    ''' Return bytes of epub book '''
+    writer = epub.EpubWriter('book.epub', book)
+    writer.process()
+
+    # Since writer.write only support writing to a file,
+    # here we implement a function to write into a byte buffer
+    writer.out = zipfile.ZipFile(buffer, 'w', zipfile.ZIP_DEFLATED)
+    writer.out.writestr('mimetype', 'application/epub+zip',
+                        compress_type=zipfile.ZIP_STORED)
+    # pylint: disable=protected-access
+    writer._write_container()
+    writer._write_opf()
+    writer._write_items()
+
+    writer.out.close()
+
+
 def main():
     txt_file = st.sidebar.file_uploader('Choose txt file', ['txt'])
     if not txt_file:
@@ -63,89 +138,38 @@ def main():
 
     reg_title = tab_chapter.text_input('title regex', '第.*章.*')
 
-    chapters = split_content(content_lines, reg_title)
+    with st.spinner("Splitting content to chapters..."):
+        chapters = split_content(content_lines, reg_title)
+
+    default_intro = chapters[0].html_content
 
     tab_chapter.dataframe(
         {
             'Index': list(range(len(chapters))),
             'Titles':  [ch.title for ch in chapters],
             'Line counts': [len(ch.content_lines) for ch in chapters],
-            'Content': ['\n'.join(ch.content_lines) for ch in chapters],
+            'Content (first 1000 lines)': [
+                '\n'.join(ch.content_lines[:1000]) for ch in chapters],
         },
         hide_index=True,
         use_container_width=True)
 
     book_title = tab_meta.text_input('Title', value=txt_file.name)
     book_author = tab_meta.text_input('Author')
-    book_intro = tab_meta.text_area(
-        'Introduction', value=chapters[0].html_content)
+    book_intro = tab_meta.text_area('Introduction', value=default_intro)
     book_cover = tab_meta.file_uploader('Book cover')
 
     if st.button('Prepare EPUB'):
-        book = epub.EpubBook()
-        book.set_title(book_title)
-        if book_cover:
-            book.set_cover(book_cover.name, book_cover.read())
+        with st.status('Preparing EPUB...', expanded=True) as status:
+            book = build_epub_book(
+                book_title, book_author, book_intro, book_cover, chapters)
 
-        if book_author:
-            book.add_author(book_author)
-
-        book.toc = []
-        spines = []
-
-        intro_filename = 'intro.html'
-        intro_title = 'Introduction'
-        intro_file = epub.EpubHtml(title=intro_title, file_name=intro_filename)
-        intro_file.content = book_intro
-        book.add_item(intro_file)
-        book.toc.append(epub.Link(intro_filename, intro_title))
-        spines.append(intro_file)
-
-        for index, chapter in enumerate(chapters[1:]):
-            ch_filename = f'ch_{index}.html'
-            ch_file = epub.EpubHtml(
-                title=chapter.title, file_name=ch_filename)
-            ch_file.content = f'<h1>{chapter.title}</h1>\n' + \
-                chapter.html_content
-            book.add_item(ch_file)
-
-            book.toc.append(epub.Link(ch_filename, chapter.title))
-
-            spines.append(ch_file)
-
-        book.add_item(epub.EpubNav())
-
-        # define CSS style
-        style = 'BODY {color: white;}'
-        nav_css = epub.EpubItem(
-            uid="style_nav",
-            file_name="style/nav.css",
-            media_type="text/css",
-            content=style,
-        )
-
-        book.add_item(nav_css)
-
-        # basic spine
-        book.spine = ['nav'] + spines
-
-        writer = epub.EpubWriter('book.epub', book)
-        writer.process()
-
-        with io.BytesIO() as buffer:
-            writer.out = zipfile.ZipFile(buffer, 'w', zipfile.ZIP_DEFLATED)
-            writer.out.writestr('mimetype', 'application/epub+zip',
-                                compress_type=zipfile.ZIP_STORED)
-
-            # pylint: disable=protected-access
-            writer._write_container()
-            writer._write_opf()
-            writer._write_items()
-
-            writer.out.close()
-
-            st.download_button('Download', buffer,
-                               file_name=book_title + '.epub')
+            with io.BytesIO() as buffer:
+                write_epub(book, buffer)
+                status.update(label='Preparing EPUB complete!',
+                              state='complete')
+                st.download_button('Download', data=buffer,
+                                   file_name=book_title + '.epub')
 
 
 main()
