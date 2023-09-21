68cd6969287742b4a3c07a013bda61fceb2275ed
Mudream <mudream4869@gmail.com>
2023-09-21 22:30:57 +0800

    Generate exe

---
+++ app_entry.py
@@ -0,0 +1,20 @@
+import os
+from streamlit.web import bootstrap
+
+if __name__ == '__main__':
+    os.chdir(os.path.dirname(__file__))
+
+    flag_options = {
+        'server.port': 0,
+        'global.developmentMode': False,
+        'browser.serverAddress': 'localhost',
+    }
+
+    bootstrap.load_config_options(flag_options=flag_options)
+    flag_options['_is_running_with_streamlit'] = True
+    bootstrap.run(
+        './README.py',
+        'streamlit run',
+        [],
+        flag_options
+    )
---
+++ build.bat
@@ -0,0 +1,1 @@
+pyinstaller app_entry.spec --clean
